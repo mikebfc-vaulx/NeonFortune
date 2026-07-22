d625436bb1931cb1e2bea70777027cb49ce29b3a
mikebfc-vaulx <mikebfc92@gmail.com>
2026-07-22 22:18:13 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const types = {
   ".css": "text/css; charset=utf-8",
   ".json": "application/json; charset=utf-8",
   ".txt": "text/plain; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
   ".png": "image/png",
   ".svg": "image/svg+xml",
 };
